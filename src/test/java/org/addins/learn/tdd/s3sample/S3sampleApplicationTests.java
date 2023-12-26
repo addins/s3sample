5c81d74c63f2e7a139687eb7d4205d779160d72b
Addinul <addins3009@gmail.com>
2023-12-26 15:47:27 +0700

    initial commit

---
+++ src/test/java/org/addins/learn/tdd/s3sample/S3sampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.addins.learn.tdd.s3sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3sampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
